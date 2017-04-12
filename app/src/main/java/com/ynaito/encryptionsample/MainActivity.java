07fccd5025d6bb243d20d001b39c2cf044537c1b
yuta naito <y.naito@suvt.co.jp>
2017-04-12 13:27:12 +0900

    add function to input and encode string

--- app/src/main/java/com/ynaito/encryptionsample/MainActivity.java
+++ app/src/main/java/com/ynaito/encryptionsample/MainActivity.java
@@ -2,12 +2,32 @@ package com.ynaito.encryptionsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private String AES_KEY = "hogehoge";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        Button EncButton = (Button) findViewById(R.id.encryptButton);
+        final TextView textView = (TextView) findViewById(R.id.text);
+        EncButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String EncText = editText.getText().toString();
+                String EncedText = EncryptionUtil.encryptAES(AES_KEY,EncText);
+                textView.setText(EncedText);
+            }
+        });
+
     }
 }
